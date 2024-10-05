cac0bb611545b1f81750744565a28e51e02bd305
Vaishakh GK <vaishakhgk2006@gmail.com>
2024-10-05 17:41:06 +0530

    feat: Need to login to display home page

--- internal/handlers/home.go
+++ internal/handlers/home.go
@@ -1,6 +1,8 @@
 package handlers
 
 import (
+	"net/http"
+
 	"github.com/FulgurCode/StellarPing/utils"
 	"github.com/FulgurCode/StellarPing/views"
 	"github.com/labstack/echo/v4"
@@ -9,6 +11,15 @@ import (
 // Home page handler
 func Home(c echo.Context) error {
 	var component = views.Home()
+	if isLoggedIn  := utils.GetSessionValue(c,"auth","isLoggedIn"); isLoggedIn != nil && isLoggedIn.(bool) {
+		
+		return utils.Render(c, component)
+	}
 
-	return utils.Render(c, component)
+	if c.Request().Header.Get("HX-Request") == "true" {
+		c.Response().Header().Set("HX-Location", "/login")
+		return c.NoContent(200)
+	}
+
+	return c.Redirect(http.StatusSeeOther, "/login")
 }
